5e8ebeac5eb7046ba90f30aba719ccdcb85b2d2c
BuildTools <unconfigured@null.spigotmc.org>
2018-07-27 00:15:56 +0300

    Перенес функционал кнопок во ВьюМодел, забиндил в лейауте функции на кнопки.

--- app/src/main/java/com/gu/team/workingtimecontrol/ui/fragments/LoginFragment.java
+++ app/src/main/java/com/gu/team/workingtimecontrol/ui/fragments/LoginFragment.java
@@ -1,15 +1,6 @@
 package com.gu.team.workingtimecontrol.ui.fragments;
 
-import androidx.databinding.DataBindingUtil;
-import androidx.lifecycle.ViewModelProviders;
-
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +8,12 @@ import android.view.ViewGroup;
 import com.gu.team.workingtimecontrol.R;
 import com.gu.team.workingtimecontrol.databinding.LoginFragmentBinding;
 import com.gu.team.workingtimecontrol.viewmodels.LoginViewModel;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 public class LoginFragment extends Fragment {
 
@@ -33,23 +30,12 @@ public class LoginFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.login_fragment, container, false);
         return binding.getRoot();
-
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
-        buttonsBinding();
-
+        binding.setModel(mViewModel);
     }
-
-    private void buttonsBinding(){
-        binding.enterButton.setOnClickListener(view ->
-                Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_timeControlFragment));
-
-        binding.regButton.setOnClickListener(view ->
-                Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_registrationFragment));
-    }
-
 }
--- app/src/main/java/com/gu/team/workingtimecontrol/viewmodels/LoginViewModel.java
+++ app/src/main/java/com/gu/team/workingtimecontrol/viewmodels/LoginViewModel.java
@@ -1,13 +1,41 @@
 package com.gu.team.workingtimecontrol.viewmodels;
 
+import android.view.View;
+
+import com.gu.team.workingtimecontrol.R;
+import com.gu.team.workingtimecontrol.common.User;
+import com.gu.team.workingtimecontrol.data.UserRepository;
+
 import androidx.lifecycle.ViewModel;
+import androidx.navigation.Navigation;
 
 public class LoginViewModel extends ViewModel {
 
     public String login;
     public String password;
     public int pinCode;
+    private UserRepository userRepository = UserRepository.getInstance();
 
+    public void enterButtonClicked(View view) {
+        if (isPasswordCorrect()) {
+            User user = userRepository.getUserByLogin(login);
+            userRepository.setCurrentUser(user);
+            Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_timeControlFragment);
+        } else
+            showErrorMessage(view);
+    }
 
+    public void registerButtonClicked(View view) {
+        Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_registrationFragment);
+    }
+
+    private boolean isPasswordCorrect() {
+        //TODO Написаль логику проверки пароля
+        return true;
+    }
+
+    private void showErrorMessage(View view) {
+        //TODO Написаль показ сообщения об ошибке
+    }
 
 }
